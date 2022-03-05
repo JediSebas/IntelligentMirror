257be4045274a34b9fa3194577a13b5685c423ab
JediSebas <sebasdolata04@gmail.com>
2022-03-05 14:41:31 +0100

    Delete Progressbar

--- app/src/main/java/com/jedisebas/inteligentmirror/ui/progressbar/ProgressbarViewModel.java
+++
@@ -1,8 +0,0 @@
-package com.jedisebas.inteligentmirror.ui.progressbar;
-
-import androidx.lifecycle.ViewModel;
-
-public class ProgressbarViewModel extends ViewModel {
-    public ProgressbarViewModel() {
-    }
-}
