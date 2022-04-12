5d4280f27384aec8281c45a05d5490051b7df55e
sebas <sebas123456665@gmail.com>
2022-04-12 18:31:37 +0200

    Password change

--- app/src/main/java/com/jedisebas/inteligentmirror/ConnectionData.java
+++ app/src/main/java/com/jedisebas/inteligentmirror/ConnectionData.java
@@ -3,9 +3,9 @@ package com.jedisebas.inteligentmirror;
 public class ConnectionData {
     public static String DB_URL = "jdbc:mysql://"+ Loggeduser.ip+"/mirror";
     public static final String USER = "user";
-    public static final String PASS = "user";
+    public static final String PASS = "zaq1@WSX";
 
     public static final int PORT = 21;
-    public static final String DIRECTORY_IN_MIRROR = "/IntelligentMirror/data/";
-    public static final String DIRECTORY_WITH_IMAGES = "/opt/lampp/htdocs/mirror/" ;
+    public static final String DIRECTORY_IN_MIRROR = "/home/user/test/";
+    public static final String DIRECTORY_WITH_IMAGES = "/var/www/html/mirror/" ;
 }
